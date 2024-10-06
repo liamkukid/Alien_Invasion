1d01d3a5bf6b31c4d25487b6bfe4cd8d2b8766fc
Дмитрий Кулик <liamkukid@gmail.com>
2024-10-06 17:59:37 +0200

    bg color and frame rate

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,8 +8,10 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Starts the main loop of the game."""
@@ -19,8 +21,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)
+
             #Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
