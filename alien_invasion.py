d563891d7dd2274d5752e7a7afc579a5e37e2927
Дмитрий Кулик <liamkukid@gmail.com>
2024-10-12 18:28:58 +0200

    building fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,7 +48,23 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet of aliens."""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_possition, y_possition):
+        new_alien = Alien(self)
+        new_alien.x = x_possition
+        new_alien.rect.x = x_possition
+        new_alien.rect.y = y_possition
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
